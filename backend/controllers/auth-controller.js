1ceb85adc56393e1392ab3395e7e70649192dacb
Kritsada Atchariyapruk <kritsada.gun123@gmail.com>
2025-06-13 12:54:25 +0700

    Update username when login can be name or email

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -3,15 +3,17 @@ const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 
 const loginHandler = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const { name, email, password } = req.body;
 
-  if (!email || !password) {
+  if (!password) {
     return res
       .status(400)
       .json({ message: "Please enter email and password." });
   }
 
-  const user = await User.findOne({ email }).select("+password");
+  const user = email
+    ? await User.findOne({ email }).select("+password")
+    : await User.findOne({ name }).select("+password");
 
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
@@ -19,7 +21,6 @@ const loginHandler = asyncHandler(async (req, res) => {
   }
 
   const userObj = user.toObject();
-  delete userObj._id;
   delete userObj.password;
   delete userObj.createdAt;
   delete userObj.updatedAt;
